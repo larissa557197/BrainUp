2ed65c9768a716927393685ceb70ea3690407237
larissa557197 <larissampmuniz@gmail.com>
2025-10-02 10:19:25 -0300

    first commit

---
+++ src/main/java/br/com/fiap/brainup/BrainupApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.brainup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainupApplication.class, args);
+	}
+
+}
