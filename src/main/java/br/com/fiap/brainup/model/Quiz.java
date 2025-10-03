78096fd843642022fce70a0e21b0a2d4439aecb0
larissa557197 <larissampmuniz@gmail.com>
2025-10-02 21:59:30 -0300

    refactor(model): update Quiz entity constructor ans structure

--- src/main/java/br/com/fiap/brainup/model/Quiz.java
+++ src/main/java/br/com/fiap/brainup/model/Quiz.java
@@ -10,48 +10,12 @@ import java.util.UUID;
 public class Quiz {
 
     private UUID id = UUID.randomUUID();
-    private List<Question> questions;
-    private List<Player> players;
-
-    // construtor
-
-
-    public Quiz(UUID id, List<Question> questions, List<Player> players) {
-        this.id = id;
-        this.questions = questions;
-        this.players = players;
-    }
+    private List<Question> questions = new ArrayList<>();
+    private List<Player> players = new ArrayList<>();
 
     // métodos
     public void addPlayer(Player player) {
-        if(player != null) {
-            this.players.add(player);
-        }
+        players.add(player);
     }
 
-    // getters e setters
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public List<Question> getQuestions() {
-        return questions;
-    }
-
-    public void setQuestions(List<Question> questions) {
-        this.questions = questions;
-    }
-
-    public List<Player> getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(List<Player> players) {
-        this.players = players;
-    }
 }
