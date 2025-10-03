67f9cc011628769c337b1471c3d0d3eb5ea5919e
larissa557197 <larissampmuniz@gmail.com>
2025-10-02 22:19:00 -0300

    refactor(model): add convenience constructor to Player

--- src/main/java/br/com/fiap/brainup/model/Player.java
+++ src/main/java/br/com/fiap/brainup/model/Player.java
@@ -8,47 +8,13 @@ import java.util.UUID;
 public class Player {
     private UUID id = UUID.randomUUID();
     private String name;
-    private int score;
-    private boolean active;
+    private int score = 0;
+    private boolean active = true;
 
     // construtor
-
-    public Player(UUID id, String name, int score, boolean active) {
-        this.id = id;
-        this.name = name;
-        this.score = score;
-        this.active = active;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Player( String name) {
         this.name = name;
     }
 
-    public int getScore() {
-        return score;
-    }
 
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
 }
